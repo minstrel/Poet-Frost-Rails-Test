d752fcf7cdffe4f17beb88acb616e65aed689afb
Donald Smith <donald@sinbissquare.com>
2018-02-27 15:45:53 -0500

    Create works working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,21 @@
 class Post < ApplicationRecord
-  def post_to_poet
-    PoetFrostAPI.create_work(name: name,
-                              datePublished: created_at.iso8601,
-                              dateCreated: created_at.iso8601,
-                              author: author,
-                              content: body 
-                              )
+  #def post_to_poet
+  #  PoetFrostAPI.create_work(name: name,
+  #                            datePublished: created_at.iso8601,
+  #                            dateCreated: created_at.iso8601,
+  #                            author: author,
+  #                            content: body 
+  #                            )
+  #end
+  extend PoetFrostConfig
+  include PoetFrostAPI
+
+  poet_frost_configure do |config|
+    config.name = :name
+    config.datePublished = :updated_at
+    config.dateCreated = :created_at
+    config.author = :author
+    config.content = :body
+    config.work_id = :workid
   end
 end
